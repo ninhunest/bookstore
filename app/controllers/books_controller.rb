6c7c2444486bf7d77e4591cf27ede3bc684832df
ToanDK <khactoan96@gmail.com>
2017-12-07 13:00:06 +0700

    Finish user sign up, sign in, sign out

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,24 +1,30 @@
 class BooksController < ApplicationController
-  before_action :load_meta_info, only: :index
+  before_action :load_meta_data, only: :index
   before_action :load_book, only: :show
+  before_action :load_meta_data_for_current_book, only: :show
 
   def index
     @books = Book.select_fields.order_by_created_at
       .page(params[:page]).per Settings.books.per_page
   end
 
-  def show
-    @authors = @book.authors
-    @related_books = @book.category.books.except_current_book(@book)
-      .select_fields.limit Settings.related_book.limit
-  end
-
   private
 
-  def load_meta_info
+  def load_meta_data
     @categories = Category.hot_categories
     @authors = Author.hot_authors
     @publishers = Publisher.hot_publishers
+  end
+
+  def load_meta_data_for_current_book
+    @comments = Comment.load_comment(@book).except_reply.select_fields
+      .order_by_created_at
+    @authors = @book.authors
+
+    if @book.category.present?
+      @related_books = @book.category.books.except_current_book(@book)
+        .select_fields.limit Settings.related_book.limit
+    end
   end
 
   def load_book
