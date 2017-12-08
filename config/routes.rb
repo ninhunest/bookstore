e5c157456f695f97e722cd9221d132cadae8b3d7
nguyen phan quang ninh <nguyenninhquang@gmail.com>
2017-12-08 12:03:35 +0700

    finished author

--- config/routes.rb
+++ config/routes.rb
@@ -22,5 +22,6 @@ Rails.application.routes.draw do
     resources :books
     resources :categories
     resources :publishers
+    resources :authors
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 20171220090042) do
 
   create_table "authors", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
     t.string "name", null: false
+    t.string "email", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
