d9611f22c6e62a4c51dd4f382e4fc3375fb69a4d
ToanDK <khactoan96@gmail.com>
2017-12-15 00:12:36 +0700

    Finish user edit comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,15 @@
 class CommentsController < ApplicationController
-  before_action :comment_params, only: %i(create)
-  before_action :load_comment, only: :destroy
-  before_action :load_book, only: :destroy
+  before_action :comment_params, only: %i(create update)
+  before_action :check_valid_role, only: %i(update destroy)
+  before_action :load_book, only: %i(update destroy)
 
   def create
     @book = Book.find_by id: params[:comment][:book_id]
     @comment = @book.comments.new comment_params
 
     if @comment.save!
-      @comments = Comment.load_comment(@book).except_reply.select_fields
-        .order_by_created_at.limit Settings.comments.limit
+      refresh
+
       respond_to do |format|
         format.js
       end
@@ -19,28 +19,67 @@ class CommentsController < ApplicationController
     end
   end
 
-  def destroy
-    if @comment.destroy!
-      @comments = Comment.load_comment(@book).except_reply.select_fields
-        .order_by_created_at.limit Settings.comments.limit
+  def update
+    if @comment.update comment_params
+      refresh
 
       respond_to do |format|
         format.js
       end
     else
-      flash[:danger] = t "comment_delete_failed"
+      flash[:danger] = t ".Update comment failed"
+      redirect_to_book
+    end
+  end
+
+  def destroy
+    if @comment.destroy!
+      refresh
+
+      respond_to do |format|
+        format.js
+      end
+    else
+      flash[:danger] = t "edit_comment_faild"
       redirect_to @book
     end
   end
 
   private
 
-  def load_comment
+  def check_valid_role
     @comment = Comment.find_by id: params[:id]
+
+    if @comment.nil?
+      flash[:danger] = t "comment_not_found"
+      redirect_to :root
+    end
+
+    return if can_edit_comment? @comment
+    flash[:danger] = t "you_can_not_edit_this_comment"
+    redirect_to_book
   end
 
   def load_book
     @book = @comment.book
+  end
+
+  def load_comment
+    @comment = Comment.find_by id: params[:id]
+
+    return if @comment
+    flash[:danger] = t "comment_not_found"
+    redirect_to_book
+  end
+
+  def refresh
+    @comments = Comment.load_comment(@book).except_reply.select_fields
+        .order_by_created_at.limit Settings.comments.limit
+  end
+
+  def redirect_to_book
+    @book = load_book
+    redirect_to @book
   end
 
   def comment_params
--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -2,4 +2,8 @@ module CommentsHelper
   def load_reply comment
     replies = Comment.reply comment
   end
+
+  def can_edit_comment? comment
+    comment.user == current_user
+  end
 end
